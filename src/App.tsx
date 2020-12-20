ba64749a2f85f106f9abb403bec4d8eda5d3b990
ConArrr <conarrr7714@gmail.com>
2020-12-20 23:34:32 +0900

    Completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from 'react'
+
+import CounterBoard from 'containers/organisms/CounterBoard'
+import ColorfulBeads from 'containers/molecules/ColorfulBeads'
 import './App.css'
 
 const App: FC = () => (
   <div className="App">
-    <header className="App-header" />
+    <header>
+      <h1>ビーズカウンター</h1>
+    </header>
+    <CounterBoard />
+    <ColorfulBeads />
   </div>
 )
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
+import { counterSlice } from 'features/counter'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+
+const store = configureStore({ reducer: counterSlice.reducer })
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </Provider>,
+  document.getElementById('root') as HTMLElement,
 )
